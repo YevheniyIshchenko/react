a2fe377a3d606acd580e94c8cd55c94a8aa33252
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-06-29 20:05:18 +0300

    task_3

---
+++ lesson_3/task_3/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Comment from './Comment.jsx';
+
+const userInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://avatars1.githubusercontent.com/avatar_url',
+};
+
+const App = () => {
+  return <Comment author={userInfo} 
+  text="Good job!" 
+  date={new Date()} />;
+};
+
+export default App;
