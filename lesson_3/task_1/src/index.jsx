a9574dc01ba1450ed4feea1b93ea6cc83bd45e6c
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-06-29 18:22:00 +0300

    task_1

--- lesson_3/task_1/src/index.jsx
+++ lesson_3/task_1/src/index.jsx
@@ -1,3 +1,16 @@
-import "./Search.jsx";
+import React from "react";
+import ReactDOM from "react-dom";
+import Search from "./Search.jsx";
 import "./index.scss";
 import "./search.scss";
+
+const rootElement = document.querySelector("#root");
+
+
+
+
+
+ReactDOM.render(<Search name="Tom"/>, rootElement);
+
+
+
