5a89f83094507f6dacdf3a382a4592887bd1626c
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-06-29 21:03:23 +0300

    hw_1

---
+++ lesson_3/hw_task1/src/Greeting.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import moment from 'moment';
+
+const Greeting = props => {
+  const age = moment(new Date()).diff(moment(props.birthDate), 'years');
+  return (
+    <div className="greeting">{`My name is ${props.name} ${props.lastName}. I'm ${age} years old`}</div>
+  );
+};
+
+export default Greeting;
