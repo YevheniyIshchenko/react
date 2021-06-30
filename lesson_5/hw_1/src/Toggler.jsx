05debe99e1e9d4c9a980447dccc973709265da85
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-06-30 19:04:14 +0300

    hw_1

--- lesson_5/hw_1/src/Toggler.jsx
+++ lesson_5/hw_1/src/Toggler.jsx
@@ -1,34 +1,31 @@
-import React from 'react';
+import React from "react";
 
-const ON = 'On';
-const OFF = 'Off'
+const ON = "On";
+const OFF = "Off";
 
 class Toggler extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            text: OFF,
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: OFF,
+    };
+  }
 
-    setButtonText = () =>{
-        this.setState({
-             text: this.state.text === OFF ? this.state.text = ON : this.state.text = OFF,
-        })
-    }
+  setButtonText = () => {
+    this.setState({
+      text: this.state.text === OFF ? ON : OFF,
+    });
+  };
 
-render(){
-    return(
-        <div className="toggler">
-            <button 
-             onClick={() => this.setButtonText()} className="button">{this.state.text}</button>
-        </div>
-    )
+  render() {
+    return (
+      <div className="toggler">
+        <button onClick={() => this.setButtonText()} className="button">
+          {this.state.text}
+        </button>
+      </div>
+    );
+  }
 }
-}
-
-
-
-
 
 export default Toggler;
--- lesson_5/hw_1/src/index.jsx
+++ lesson_5/hw_1/src/index.jsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import Toggler from './Toggler.jsx'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Toggler from "./Toggler.jsx";
 
-
-const rootElement = document.querySelector('#root');
+const rootElement = document.querySelector("#root");
 
 ReactDOM.render(<Toggler />, rootElement);
-
-
-
