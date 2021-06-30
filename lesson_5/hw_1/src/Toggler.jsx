814c3eb8454b8f662952cc0d3452c6852b2c7c2d
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-06-30 19:25:44 +0300

    hw1

--- lesson_5/hw_1/src/Toggler.jsx
+++ lesson_5/hw_1/src/Toggler.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const ON = "On";
-const OFF = "Off";
+// const ON = "On";
+// const OFF = "Off";
 
 class Toggler extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: OFF,
+      text: "Off",
     };
   }
 
@@ -15,24 +15,28 @@ class Toggler extends React.Component {
     // this.setState({
     //   text: this.state.text === OFF ? ON : OFF,
     // });
-    if (this.state.text === OFF) {
-      this.setState({
-        text: ON,
-      });
-    } else {
-      this.setState({
-        text: OFF,
-      });
+    if(this.state.text === 'Off'){
+        this.setState({
+            text: 'On',
+        })
+    }else{
+        this.setState({
+            text: 'Off',
+        })
     }
   };
 
   render() {
     return (
-      <button onClick={() => this.setButtonText()} className="button">
-        {this.state.text}
-      </button>
+      <div className="toggler">
+        <button onClick={() => this.setButtonText()} className="button">
+          {this.state.text}
+        </button>
+      </div>
     );
   }
 }
 
 export default Toggler;
+
+
