9c1f7ee35a3ae250469659ad213759ff3765b5ab
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-07-05 14:46:50 +0300

    task1

--- lesson_7/task_1/src/NumbersList.jsx
+++ lesson_7/task_1/src/NumbersList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const NumersList = (props) => {
+const NumbersList = (props) => {
   return (
     <ul>
       {props.numbers.map((number) => (
@@ -11,4 +11,4 @@ const NumersList = (props) => {
 };
 
 
-export default NumersList;
+export default NumbersList;
--- lesson_7/task_1/src/index.jsx
+++ lesson_7/task_1/src/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import NumersList from "./NumersList.jsx";
+import NumbersList from "./NumbersList.jsx";
 
 const rootElement = document.querySelector("#root");
 
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 
-ReactDOM.render(<NumersList numbers={numbers} />, rootElement);
+ReactDOM.render(<NumbersList numbers={numbers} />, rootElement);
