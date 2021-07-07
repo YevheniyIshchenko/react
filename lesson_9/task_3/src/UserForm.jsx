1ea83162473426b4c6febf1bac1ea4c54c7d845b
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-07-07 16:22:30 +0300

    task3

--- lesson_9/task_3/src/UserForm.jsx
+++ lesson_9/task_3/src/UserForm.jsx
@@ -1,42 +1,33 @@
 import React from "react";
 
 class Search extends React.Component {
-  state = {
-    name: "",
-    student: "",
-    occupation: "",
-    about: "",
-  };
-
-  handleChange = (event) => {
-    const { name, value, checked, type } = event.target;
-    const val = type === "checkbox" ? checked : value;
-    this.setState({
-      [name]: val,
-    });
-  };
-
   handleSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state);
+    const formData = [...new FormData(this.formRef)].reduce(
+      (acc, [name, value]) => ({ ...acc, [name]: value }),
+      {}
+    );
+
+    console.log(formData);
+  };
+
+  setRef = (node) => {
+    this.formRef = node;
   };
 
   render() {
     return (
-      <form className="login-form" onSubmit={this.handleSubmit}>
+      <form
+        ref={this.setRef}
+        className="login-form"
+        onSubmit={() => this.props.onSubmit(this.state)}
+      >
         <h1 className="form-title">Profile</h1>
         <div className="form-control">
           <label className="form-label" htmlFor="name">
             Name
           </label>
-          <input
-            className="form-input"
-            value={this.state.name}
-            type="text"
-            id="name"
-            name="name"
-            onChange={this.handleChange}
-          />
+          <input className="form-input" type="text" id="name" name="name" />
         </div>
         <div className="form-control">
           <label className="form-label" htmlFor="student">
@@ -44,23 +35,16 @@ class Search extends React.Component {
           </label>
           <input
             className="form-input"
-            value={this.state.student}
             type="checkbox"
             id="student"
             name="student"
-            onChange={this.handleChange}
           />
         </div>
         <div className="form-control">
           <label className="form-label" id="occupation" htmlFor="occupation">
             Occupation
           </label>
-          <select
-            name="occupation"
-            value={this.state.occupation}
-            className="form-input"
-            onChange={this.handleChange}
-          >
+          <select name="occupation" className="form-input">
             <option value="london">London</option>
             <option value="new-york">New York</option>
             <option value="sidney">Sidney</option>
@@ -73,7 +57,6 @@ class Search extends React.Component {
           </label>
           <textarea
             name="about"
-            value={this.state.about}
             className="form-input"
             onChange={this.handleChange}
           />
