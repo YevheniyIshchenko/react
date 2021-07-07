3ce77399397cbe8e4865cf4a6c0242ff4d19abee
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-07-07 16:02:29 +0300

    task2

--- lesson_9/task_2/src/UserForm.jsx
+++ lesson_9/task_2/src/UserForm.jsx
@@ -16,14 +16,12 @@ class Search extends React.Component {
     });
   };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state);
-  };
-
   render() {
     return (
-      <form className="login-form" onSubmit={this.handleSubmit}>
+      <form
+        className="login-form"
+        onSubmit={() => this.props.onSubmit(this.state)}
+      >
         <h1 className="form-title">Profile</h1>
         <div className="form-control">
           <label className="form-label" htmlFor="name">
