92fdfa6b59015ec4d97abee653d3acb051787fdf
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-07-14 16:15:44 +0300

    hw2

---
+++ lesson_14/hw_2/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import Clock from "./Clock.jsx";
+
+const App = () => {
+  const [btnStatus, setBtn] = useState(true);
+  return (
+    <>
+      <button className="btn" onClick={() => setBtn(!btnStatus)}>
+        Toggle
+      </button>
+      <div>{btnStatus && <Clock location="New York" offset={-4} />}</div>
+      <div>{btnStatus && <Clock location="Kyiv" offset={3} />}</div>
+      <div>{btnStatus && <Clock location="London" offset={0} />}</div>
+    </>
+  );
+};
+
+export default App;
---
+++ lesson_14/hw_2/src/Clock.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+
+const Clock = ({ location, offset }) => {
+  const [clocks, setClock] = useState(new Date());
+
+  useEffect(() => {
+    const clock = () => {
+      setInterval(() => {
+        setClock(new Date());
+      }, 1000);
+    };
+
+    clock();
+    return () => {
+      clearInterval(clock);
+    };
+  }, []);
+
+  return (
+    <div className="clock">
+      <div className="clock__location">{location}</div>
+      <div className="clock__time">
+        {moment(clocks).utcOffset(offset).format("LTS")}
+      </div>
+    </div>
+  );
+};
+
+export default Clock;
